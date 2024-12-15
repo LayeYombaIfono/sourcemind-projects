da3246a4fdb70c70b02066abdeff3314a85da553
Laye Yomba Ifono <layeyomba12@gmail.com>
2024-12-15 09:26:53 +0000

    mise à jour

--- gestiondeparkautomobile/gestiondeparkautomobile/src/main/java/com/application/de/gestion/parking/gestiondeparkautomobile/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.application.de.gestion.parking.gestiondeparkautomobile.repository;
-
-import com.application.de.gestion.parking.gestiondeparkautomobile.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-
-}
